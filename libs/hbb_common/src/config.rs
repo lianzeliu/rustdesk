814a66d08c5dc7e1055425cfa62fe7686019aac7
unknown <88493376@qq.com>
2026-05-10 22:04:27 +0800

    123

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -533,14 +533,10 @@ impl Config2 {
         let mut config = Config::load_::<Config2>("2");
         let mut store = false;
         if let Some(mut socks) = config.socks {
-        if !config.options.contains_key("verification-method") {
-            config.options.insert("verification-method".to_string(), "use-permanent-password".to_string());
-            store = true;
-        }    
-        if !config.options.contains_key("allow-remote-config-modification") {
+            if !config.options.contains_key("allow-remote-config-modification") {
                 config.options.insert("allow-remote-config-modification".to_string(), "Y".to_string());
                 store = true;
-            }    
+            }
             let (password, _, store2) =
                 decrypt_str_or_original(&socks.password, PASSWORD_ENC_VERSION);
             socks.password = password;
